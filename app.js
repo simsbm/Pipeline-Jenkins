981d25f32d402377a27f498e8aa02f5cff38d91a
simsbm <morelbrel@gmail.com>
2026-01-08 18:17:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello Morel! Votre pipeline est maintenant ok ');
+  res.send('Hello Morel SIMO! Votre pipeline est maintenant ok ');
 });
 
 app.listen(port, () => {
